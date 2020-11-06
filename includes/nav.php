63ea308f4b8e45665e41a59ba5886e98fab11f3b
IsaacGS17 <71747998+IsaacGS17@users.noreply.github.com>
2020-11-05 22:48:46 -0700

    Add files via upload

---
+++ includes/nav.php
@@ -0,0 +1,9 @@
+	<nav>
+		<ul>
+			<a href='index.php'><li>Inicio</li></a>
+			<a href='p1_nava_20.php'><li>Problema 1</li></a>
+			<a href='p2_nava_20.php'><li>Problema 2</li></a>
+			<a href='p3_nava_20.php'><li>Problema 3</li></a>
+			<a href='http://localhost/daweb/P7_Granados/UII_p7nava-main/index.php'><li>Volver</li></a>
+		</ul>
+	</nav>
